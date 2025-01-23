61a87ebf96699be31abba5b909d0ccbbf1aef0fc
lkhyun-0 <a01024705367@gmail.com>
2025-01-23 18:27:08 +0900

    frist commit

---
+++ src/main/java/com/aws/carepoint/CarepointApplication.java
@@ -0,0 +1,13 @@
+package com.aws.carepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarepointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarepointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aws/carepoint/CarepointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.carepoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarepointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
