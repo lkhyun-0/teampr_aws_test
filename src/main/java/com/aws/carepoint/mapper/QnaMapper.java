24ed859bec6151a25580419ae7231a17c0c85e54
eseul2 <dltmfsus@naver.com>
2025-02-18 17:42:54 +0900

    qna 보드pk값 수정 메인페이지 qna게시물 출력 확인

--- src/main/java/com/aws/carepoint/mapper/QnaMapper.java
+++ src/main/java/com/aws/carepoint/mapper/QnaMapper.java
@@ -99,7 +99,7 @@ public interface QnaMapper {
 
     @Select("SELECT * " +
             "FROM article " +
-            "WHERE board_pk = 1 " +
+            "WHERE board_pk = 3 " +
             "AND user_pk = #{userPk} " +
             "ORDER BY reg_date DESC " +
             "LIMIT 5")
